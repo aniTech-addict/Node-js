10a6d83762635772cf775c32c518600037cfc83e
aniTech-addict <kirti7666anand15@gmail.com>
2026-03-12 14:20:44 +0530

    chore: added comments for each emitter methods

--- eventEmitters/events.js
+++ eventEmitters/events.js
@@ -1,15 +1,31 @@
 class EventEmitter {
-
+	
+	/**
+	 * Main object to handle all active listieners 
+	 */
 	listiener = {};
 
+
 	addEventListiener (eventName, fn) {}
+	/*
+	 * Push the object to the listiener object
+	 */
 	on() {}
 
+	/*
+	 * remove the object from thr listiener object
+	 */
 	removeEventListiener (eventName, fn) {}
 	off () {}
-
+	
+	/* 
+	 * Push and remove the object after one emit
+	 * */
 	once() {}
-
+	
+	/*
+	 * Emit the event
+	 * */
 	emit(eventName, ...args) {}
 
 	listienerCount() {};
