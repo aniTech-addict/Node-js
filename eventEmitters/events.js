346f4dd1cae3dae620e69c9b654dd9e00f9f7549
aniTech-addict <kirti7666anand15@gmail.com>
2026-03-12 22:29:14 +0530

    feat: implement addEventListieners, removeEventListieners and once methods

--- eventEmitters/events.js
+++ eventEmitters/events.js
@@ -3,33 +3,67 @@ class EventEmitter {
 	/**
 	 * Main object to handle all active listieners 
 	 */
-	listiener = {};
+	listieners = {};
 
-
-	addEventListiener (eventName, fn) {}
 	/*
+	 * Takes an event and the callback function to be executed 
+	 * if already exists then return the array else register the event and return empty array. 
+	 */
+	addEventListiener (eventName, fn) {
+		this.listieners[eventName] = this.listieners[eventName] || [];
+		this.listieners[eventName].push(fn);
+		return this;
+	}
+	/*
+	 * alias to the addEventListiener
 	 * Push the object to the listiener object
 	 */
-	on() {}
+	on(event, fn) {
+		return this.addEventListiener(event, fn);
+	}
 
 	/*
-	 * remove the object from thr listiener object
-	 */
-	removeEventListiener (eventName, fn) {}
-	off () {}
+	* remove the listiener(eventName) from the events arrays 
+	*/
+	removeEventListiener (eventName, fn) {
+		let lis = this.listiener[eventName];
+		if(!lis) return this;
+		for( let i=listtiener.length()-1 ; i>=0 ; i++) {
+			if (lis[i] === fn) {
+				lis.splice(i,1);
+			}
+		}
+		return this;
+		
+	}
+	// alias for removeEventEmitter
+	off (eventName, fn) {
+		return this.removeEventListiener(eventName, fn);
+	}
 	
 	/* 
 	 * Push and remove the object after one emit
 	 * */
-	once() {}
+	once() {
+		
+	}
 	
 	/*
 	 * Emit the event
 	 * */
-	emit(eventName, ...args) {}
+	emit(eventName, ...args) {
+		let funs = this.listieners[eventName];
+		if (!funs) return false;
+		funs.forEach((fn)=>{
+			fn(...args);
+		});
+
+	}
 
 	listienerCount() {};
 
-	rawListieners{};
+	rawListieners() {};
 
 };
+
+module.exports = EventEmitter;
