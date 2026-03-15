f2e66d45684285dd33b9f80ac991c0cad2d30df8
aniTech-addict <kirti7666anand15@gmail.com>
2026-03-15 10:49:20 +0530

    feat: solving race conditions by setting timeOut for eventlistiener

--- fileSystems/creatingFileManager/app.js
+++ fileSystems/creatingFileManager/app.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs/promises');
 const {Buffer} = require('node:buffer');
-
-
+let debouncerTimer;
 
 (async() => {	
 	
@@ -9,12 +8,17 @@ const {Buffer} = require('node:buffer');
 	try {
 		const  fileHandler = await fs.open('command.txt', 'r')
 		
-		 fileHandler.on('change', () => captureContent(fileHandler));
-		
+		fileHandler.on('change', async() => {
+			clearTimeout(debouncerTimer);
+			debouncerTimer = setTimeout(async()=> {
+				await captureContent(fileHandler)
+			},100);
+		});
 		const watcher = fs.watch("command.txt");
 		for await (const event of watcher) {
 			if (event.eventType == 'change') {
 				fileHandler.emit('change');
+				console.log("Some change in file")
 			}
 	
 		}
@@ -32,8 +36,61 @@ const captureContent =  async(fileHandler)=> {
 	const offset = 0;
 	const position = 0;
 	const content = await fileHandler.read(buffer, offset, fileSize, position); 
-	console.log(content.buffer.toString('utf-8'));
-	
+	//	console.log(content.buffer.toString('utf-8'));
+	await makeDecisions (content.buffer.toString('utf-8'));
 };
 
+async function makeDecisions ( filecontent ) {
+	 console.log("File Content: ",filecontent);
+	// create file
+	// delete file
+	// write content to a file
+	// read content from a file
+	// append content to a file
+	// erase content from a file
+	const argument = filecontent.split("$").pop();
+	if (!argument) return;
+	if (filecontent.includes("create file")) {
+
+		await createFile(argument);
+		
+	} else if ( filecontent.includes("delete file")) {
+		await deleteFile(argument);
+	}	
+}
+
+async function fileExists(filename) {
+	let fileHandler;
+	try {
+		fileHandler = await fs.open(filename, 'r');
+		return true;
+	} catch (error) {
+		if (error.code === "ENOENT") return false;
+	} finally {
+		if (fileHandler) await fileHandler.close();
+	}
+}
+
+ async function createFile (filename) {
+	const result = await fileExists(filename);
+	if (result) {
+		console.log("File Already Exists");
+		return;
+	}
+	if ( !result ) { 
+		
+		// file does not exit, create file
+		try {
+			const newFileHandler = await fs.open(filename, 'w');
+			console.log(`File named ${filename} created successfully`);
+			await newFileHandler.close();
+		} catch (error) {
+			console.log("Error creating file", error)
+		}
+	}
+}
+
+async function deleteFile (filename) {
+	
+}
 
